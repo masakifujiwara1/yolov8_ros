80e8bce0753362ce9daaf3c0cbd7eab9568343dc
masakifujiwara1 <fmasaki2020@gamil.com>
2024-06-05 17:42:30 +0900

    Add topic

--- yolov8_ros/yolov8_ros/bev.py
+++ yolov8_ros/yolov8_ros/bev.py
@@ -5,12 +5,15 @@ from collections import defaultdict
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from yolov8_msgs.msg import DetectionArray
+from geometry_msgs.msg import PoseArray
+from visualization_msgs.msg import Marker, MarkerArray
 
 class LaserListener(Node):
     def __init__(self):
         super().__init__('laser_listener')
         self.scan_sub = self.create_subscription(
             LaserScan,
+            # '/velodyne_2dscan',
             '/velodyne_2dscan_high_beams',
             self.callback,
             10)
@@ -19,11 +22,14 @@ class LaserListener(Node):
             '/yolo/tracking',
             self.callback_yolo,
             10)
+        self.marker_array_pub = self.create_publisher(MarkerArray, 'detect_human', 10)
         self.dicts = defaultdict(lambda: {'id': 0, 'score': 0, 'theta': 0, 'x': 0, 'y': 0, 'size_x': 0, 'size_y': 0})
         self.scan = LaserScan()
+        self.marker_array = MarkerArray()
 
     def callback_yolo(self, data):
         self.dicts.clear()
+        self.marker_array = MarkerArray()
         for idx, i in enumerate(data.detections):
             if i.class_id == 0:
                 self.dicts[idx]['id'] = i.id
@@ -59,15 +65,20 @@ class LaserListener(Node):
                 x, y = self.calc_xy(angle, distance)
                 # if score >= 0.75:
                 # self.get_logger().info(f'Angle(rad): {angle}, Index: {index}, Distance(m): {distance}, x: {x}, y: {y}, tracking_id: {id_}')
-                # self.get_logger().info(f'Angle(rad): {angle}, Index: {index}, Distance(m): {distance}, tracking_id: {id_}, size_x: {size_x}, size_y: {size_y}')
+                self.get_logger().info(f'Angle(rad): {angle}, Index: {index}, Distance(m): {distance}, tracking_id: {id_}, size_x: {size_x}, size_y: {size_y}')
 
-                if distance == float('inf'):
-                    self.get_logger().info(f'Angle(rad): {angle}, Index: {index}, tracking_id: {id_}, score: {score}, size_x: {size_x}, size_y: {size_y}')
+                # if distance == float('inf'):
+                #     self.get_logger().info(f'Angle(rad): {angle}, Index: {index}, tracking_id: {id_}, score: {score}, size_x: {size_x}, size_y: {size_y}, items: {len(self.dicts.items())}')
 
                 self.dicts[key]['x'] = x
                 self.dicts[key]['y'] = y
+
+                if distance != float('inf'):
+                    self.add_marker(id_, x, y)
             else:
                 self.get_logger().warn('計算されたインデックスが範囲外です')
+
+        self.publish_marker_array()
 
         print("-" * 120)
 
@@ -81,6 +92,27 @@ class LaserListener(Node):
 
     def is_valid_index(self, i, length):
         return 0 <= i < length
+
+    def add_marker(self, id_, x, y):
+        point = Marker()
+        point.header.frame_id = 'velodyne'
+        point.ns = "point"
+        point.id = int(id_)
+        point.type = Marker.SPHERE
+        point.action = Marker.ADD
+        point.pose.position.x = x
+        point.pose.position.y = y
+        point.pose.position.z = 0.0
+        point.scale.x = point.scale.y = point.scale.z = 1.0
+        point.color.r = 0.0
+        point.color.g = 1.0
+        point.color.b = 0.0
+        point.color.a = 1.0
+        point.lifetime = rclpy.duration.Duration(seconds = 0.001).to_msg()
+        self.marker_array.markers.append(point)
+    
+    def publish_marker_array(self):
+        self.marker_array_pub.publish(self.marker_array)
 
 def main(args=None):
     rclpy.init(args=args)
